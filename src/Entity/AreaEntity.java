8d919c1f0b992b7f458b99a75a658e0700728f31
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-08-08 00:50:15 +0300

    Реализация сущностей UserEntity и AreaEntity.

--- src/Entity/AreaEntity.java
+++ src/Entity/AreaEntity.java
@@ -6,9 +6,51 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "areas")
+@Table(name = "Areas")
 public class AreaEntity {
 	@Id
-	@Column(name = "area_id")
-	private Integer areaId;
+	@Column(name = "idArea")
+	private Long idArea;
+	@Column(name = "areaName")
+	private String areaName;
+	@Column(name = "areaAccont")
+	private int areaAccount;
+	@Column(name = "areaBalance")
+	private int areaBalance;
+
+	public AreaEntity() {
+
+	}
+
+	public Long getIdArea() {
+		return idArea;
+	}
+
+	public void setIdArea(Long idArea) {
+		this.idArea = idArea;
+	}
+
+	public String getAreaName() {
+		return areaName;
+	}
+
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+
+	public int getAreaAccount() {
+		return areaAccount;
+	}
+
+	public void setAreaAccount(int areaAccount) {
+		this.areaAccount = areaAccount;
+	}
+
+	public int getAreaBalance() {
+		return areaBalance;
+	}
+
+	public void setAreaBalance(int areaBalance) {
+		this.areaBalance = areaBalance;
+	}
 }
--- src/Entity/UserEntity.java
+++ src/Entity/UserEntity.java
@@ -6,17 +6,57 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name = "Users")
 public class UserEntity {
 	@Id
-	@Column(name = "user_id")
-	private Integer userId;
-	@Column(name = "firstname")
-	private String firstname;
-	@Column(name = "lastname")
-	private String lastname;
-	@Column(name = "username")
-	private String username;
+	@Column(name = "idUser")
+	private Long idUser;
+	@Column(name = "firstName")
+	private String firstName;
+	@Column(name = "lastName")
+	private String lastName;
+	@Column(name = "login")
+	private String login;
 	@Column(name = "password")
 	private String password;
+
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
