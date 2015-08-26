6cf583bc2d1ad46ad28a0cb9c142f075cded092f
Andrei KeduK <andrei_keduk@mail.ru>
2015-08-26 23:52:09 +0300

    Update findinf bids of user.

--- src/main/java/sjc/investFund/repository/BidRepository.java
+++ src/main/java/sjc/investFund/repository/BidRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.CriteriaSpecification;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
 import org.springframework.stereotype.Repository;
 
 import sjc.investFund.dao.BidDao;
@@ -14,24 +16,34 @@ import sjc.investFund.model.Bid;
 import sjc.investFund.model.Project;
 import sjc.investFund.model.User;
 
-
 @Repository
 public class BidRepository extends AbstractHibernateDao<Bid, Integer> implements
 		BidDao {
 
 	@Override
 	public Bid getProjectBid(Project project) {
-		Criteria criteria = getSession().createCriteria(Bid.class, "bids").add(Restrictions.eq("project", project));
-		return (Bid)criteria.uniqueResult();
+		Criteria criteria = getSession().createCriteria(Bid.class, "bids").add(
+				Restrictions.eq("project", project));
+		return (Bid) criteria.uniqueResult();
 	}
 
+	
 	@Override
 	public List<Bid> getBidsByUser(User user) {
-		Criteria criteria = getSession().createCriteria(Bid.class, "bids");
-		criteria.createAlias("project", "bid_project");
-		criteria.createAlias("user", "project_user");
-		criteria.add(Restrictions.eq("project_user", user));
-		return null;
+
+//		List<Bid> bids = getSession()
+//				.createQuery(
+//						"from Bid as b left join b.project as p left join p.user as u where u = ? ")
+//				.setEntity(0, user).list();
+		
+
+		 Criteria criteria = getSession().createCriteria(Bid.class, "Bids");
+		 criteria.createAlias("project","bid_project",JoinType.LEFT_OUTER_JOIN);
+		 criteria.createAlias("bid_project.user","project_user",JoinType.LEFT_OUTER_JOIN);
+		 criteria.add(Restrictions.eq("project_user.login", user.getLogin()));
+//		 criteria.setProjection(Projections.property("bids"));
+
+		return (List<Bid>)criteria.list();
 	}
 
 }
